41dd12e03e9091678e9b4e40331e6f4690ae1e6f
CamilaSouza_s <99446497+CamilasouzaDS@users.noreply.github.com>
2022-04-21 22:20:44 -0300

    Adding a featuring of a minigame kind HANGMAN learned

---
+++ list.py
@@ -0,0 +1,35 @@
+secret = 'Camila'
+letter = []
+chances = 3
+
+while True:
+    letra = input('Choose one character: ')
+
+    if len(letra) > 1:
+        print('Oh sorry, only one character')
+        continue
+    letter.append(letra)
+
+    if letra in secret:
+        print(f'Uhu {letra} is one of the characters in the secret')
+    else:
+        print('Sorry, this letter don´t exist in secret')
+        letter.pop()
+
+    temporary_word = ''
+    for secret_letter in secret:
+        if secret_letter in letter:
+            temporary_word += secret_letter
+        else:
+            temporary_word += '*'
+
+    if temporary_word == secret:
+        print(f"Mama call, congratulations, secret word was {secret}")
+    else:
+        print(f"secret word is {temporary_word}")
+
+    if letra not in secret and chances > 0:
+        chances -= 1
+        print(f'You still have {chances}')
+    else:
+        print('No more chances, sorry :(')
